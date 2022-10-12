991065b2a31f0bbe34c2ea8af12c7520725d9d7c
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-12 19:30:09 +0300

    Added name to paths and <p> elements to the render

--- products/views.py
+++ products/views.py
@@ -1,12 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+
 from .models import Product
+
 # Create your views here.
 def get_home(request):
    return HttpResponse("<h1>Welcome</h1>") 
 
 def get_product(request, product_id):
     product = Product.objects.get(id=product_id) 
-    return HttpResponse(f'''Name: {product.name}
-    Price: {product.price}
-    Description: {product.description}''')
+    return HttpResponse(f'''
+    <p>Name: {product.name}</p>
+    <p>Price: {product.price}</p>
+    <p>Description: {product.description}</p>
+    ''')
