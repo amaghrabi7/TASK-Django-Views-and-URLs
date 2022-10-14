e2ef0e4e8060b248d818743752b1f9cbe99ebfa6
amaghrabi7 <abdul.maghrabi@gmail.com>
2022-10-14 13:22:15 +0300

    finished the detail page in templates task

--- products/views.py
+++ products/views.py
@@ -8,9 +8,17 @@ def get_home(request):
    return HttpResponse("<h1>Welcome</h1>") 
 
 def get_product(request, product_id):
-    product = Product.objects.get(id=product_id) 
-    return HttpResponse(f'''
-    <p>Name: {product.name}</p>
-    <p>Price: {product.price}</p>
-    <p>Description: {product.description}</p>
-    ''')
+    product = Product.objects.get(id=product_id)
+    context = {
+      "product": {
+         "name":product.name,
+          "description": product.description,
+           "price": product.price
+      },
+      }
+    return render(request, "product_detail.html", context)
+   #  return HttpResponse(f'''
+   #  <p>Name: {product.name}</p>
+   #  <p>Price: {product.price}</p>
+   #  <p>Description: {product.description}</p>
+   #  ''')
